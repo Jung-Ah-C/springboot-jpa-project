15c90c84390b3f7d0da3382817c9b93784ff4621
Jung-Ah-C <wjddk718@gmail.com>
2022-07-13 14:48:46 +0900

    엔티티 설계 시 주의점에 맞게 코드 변경, 연관관계 편의 메서드 추가

--- src/main/java/com/jpabook/jpashop/domain/Order.java
+++ src/main/java/com/jpabook/jpashop/domain/Order.java
@@ -17,14 +17,14 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne // 다대일 관계, 한 명의 주문자가 여러개의 주문을 할 수 있음
+    @ManyToOne(fetch = FetchType.LAZY) // 다대일 관계, 한 명의 주문자가 여러개의 주문을 할 수 있음
     @JoinColumn(name = "member_id") // FK name 지정
     private Member member;
 
     @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "delivery_id") // 일대일 관계일 경우에는 양쪽 어느 곳에 FK를 둬도 상관 없음, 많이 조회하는 쪽에 두는 것이 효율적임
     private Delivery delivery;
     
@@ -32,4 +32,21 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문상태 : order, cancel
+
+    // == 연관관계 편의 메서드 (양방향 일 때 사용) ==
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
 }
