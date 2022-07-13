d9e5707eb68517934381ee94905d3fc3d68c7e39
Jung-Ah-C <wjddk718@gmail.com>
2022-07-13 16:04:17 +0900

    회원 리포지토리 개발

--- src/main/java/com/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/com/jpabook/jpashop/JpashopApplication.java
@@ -3,7 +3,7 @@ package com.jpabook.jpashop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // 이 어노테이션이 붙어 있으면, 해당 패키지와 패키지 안의 하위 요소들을 컴포넌트 스캔을 함, 스프링 빈으로 자동 등록
 public class JpashopApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/com/jpabook/jpashop/repository/MemberRepository.java
@@ -5,19 +5,35 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
-@Repository
+@Repository // 컴포넌트 스캔에 의해서 자동으로 스프링 빈에 등록되어서 관리됨
 public class MemberRepository {
 
+    // 스프링이 EntityManager를 만들어서 주입해줌
     @PersistenceContext
     private EntityManager em;
 
-    public Long save(Member member) {
+    // 회원 등록
+    public void save(Member member) {
         em.persist(member);
-        return member.getId();
     }
 
-    public Member find(Long id) {
+    // 회원 id로 조회
+    public Member findOne(Long id) {
         return em.find(Member.class, id);
+    }
+
+    // 전체 회원 조회
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class) // 첫번째 : JPQL 작성, 두번째 : 반환 타입 명시
+                .getResultList(); // 조회한 결과를 List로 만들어줌
+    }
+
+    // 회원 이름으로 조회
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
     }
 }
